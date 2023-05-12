bebe2c030518a8f95400824cc68b628b82249cec
prithoomedhi <prithoo.medhi@mslate.ai>
2023-05-13 02:31:08 +0530

    disclaimers and argument output

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ fn main() -> libs::structures::models::Output{
     let h_0: f64 = libs::utils::hubble_handler::get_h0(data);
     let age: f64 = libs::utils::hubble_handler::calculate_age(h_0);
 
-    println!("Hubble Constant: {} km/s/Mpc", libs::utils::misc_handler::format_f64(h_0));
+    println!("DISCLAIMER: The values calculated by this program are based on the data provided by the user.");
+    println!("DISCLAIMER: There might be floating point precision errors.");
+
+    println!("Data file: {}", file_path);
+    println!("Hubble Constant was calculated to be: {} km/s/Mpc", libs::utils::misc_handler::format_f64(h_0));
     println!("Age of the Universe: {} years", libs::utils::misc_handler::format_f64(age));
 
     let output: libs::structures::models::Output = libs::structures::models::Output::create(h_0, age);
